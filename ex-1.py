a059249e9054ddd3072c7b0fe9ff15852f20d6d5
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-11 08:01:51 +0700

    Update ex-1.py

--- ex-1.py
+++ ex-1.py
@@ -1,7 +1,19 @@
-a = []
-b = []
-for i in range(10):
-    a.append(int(input()))
-for j in range(8):
-    b.append(a[j] + a[j + 1] + a[j + 2])
-print(b)
+def get_numbers() -> list:
+    numbers = []
+    for i in range(10):
+        n = int(input())
+        numbers.append(n)
+    return numbers
+
+def make_new_list(nums) -> list:
+    new_list = []
+    for i in range(1, len(nums) - 1):
+        new_list.append(nums[i - 1] + nums[i + 1])
+    return new_list
+
+def main():
+    numbers = get_numbers()
+    result = make_new_list(numbers)
+    print("Новый список:", result)
+
+main()
