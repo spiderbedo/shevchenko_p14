75fe26cd6d072d39fc2d4e908b2c1adae4d7cb90
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-11-11 08:26:19 +0700

    Fix type hint for remove_three function parameter

--- ex-2.py
+++ ex-2.py
@@ -5,7 +5,7 @@ def read_input() -> list:
     return [int(i) for i in line.split()]
 
 
-def remove_three(numbers: list) -> list:
+def remove_three(numbers) -> list:
     if numbers.count(3) != 1:
         raise ValueError("Исходный список должен содержать ровно одно значение 3.")
     lol = numbers.index(3)
